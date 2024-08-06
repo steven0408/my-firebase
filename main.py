e2eeb4716a3388e571c29cdf8ab36752631325d6
steven0408 <91448178+steven0408@users.noreply.github.com>
2024-08-06 15:26:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-from rw_data import fetch_data, write_data
+import time
+import firebase_admin
+from firebase_admin import credentials, db
 
-# 调用 fetch_data 并打印结果
-if __name__ == "__main__":
-    key = fetch_data()
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('adminsdk.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://woolen-firebase-test1-default-rtdb.firebaseio.com/'
+})
+
+# Reference to the database service
+ref = db.reference('texts')
+
+# Function to fetch data from Firebase
+def fetch_data():
+    data = ref.get()
+    if data:
+        content = data['chat']['content']
+        source = data['chat']['source']
+    else:
+        print("No data found")
+
+# 定义数据变化处理函数
+def on_data_change(event):
+    data = event.data
+    if data['chat']['source'] != 'python':
+        fetch_data()
+        write_data('pp')
+
+def write_data(new_data):
+    # 向 Firebase 实时数据库写入新数据
+
+    ref = db.reference(f'texts')
     new_data = {
-        'content': 'This is a new entry',
-        'source': 'python'
+        'chat':{
+            'content': new_data,
+            'source': 'python'
+        }
     }
-    write_data(key, new_data)
+    ref.update(new_data)  # 使用 update 更新数据
+
+# Example usage
+if __name__ == "__main__":
+
+    while True:
+    # 添加监听器
+        print("Listening for new data...")
+        ref.listen(on_data_change)
+        time.sleep(1)
